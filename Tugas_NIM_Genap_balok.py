704b7b497c1eee1d9d3f54cd144036052c32eb7b
M. ROBBI AL HAKIM <143859725+Alhabi19@users.noreply.github.com>
2023-09-03 14:08:29 +0700

    Add files via upload
    clear

---
+++ Tugas_NIM_Genap_balok.py
@@ -0,0 +1,9 @@
+panjang = int(input("masukan panjang balok = "))
+lebar = int(input("masukan lebar balok = "))
+tinggi = int(input("masukan tinggi balok = "))
+
+volume =int(panjang*lebar*tinggi)
+print("volume balok adalah =", volume)
+
+
+
